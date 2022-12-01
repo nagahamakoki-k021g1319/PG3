d91580742634dd0757bdb6c2b80ea022ff5bf9c6
nagahama_kouki <k021g1319@g.neec.ac.jp>
2022-12-01 14:03:47 +0900

    add

--- main.cpp
+++ main.cpp
@@ -8,43 +8,43 @@ using namespace std;
 int main() {
 
 
-    std::list<const char*> yamanote = {
+    std::list<const char*> yamanotesen = {
         "Tokyo","Kanda","Akihabara","Okatimati","Ueno",
         "Uguisudani","Nippori","Tabata","Komagome","Sugamo",
-        "Ootuka","Ikebukuro","Mejiro","Takadanobaba","shinookubo",
+        "Ootuka","Ikebukuro","Mejiro","Takadanobaba","sinokubo",
         "Shinjuku","Yoyogi","Harajuku","Shibuya","Ebisu","Meguro",
         "Gotanda","Oosaki","Shinagawa","Tamati","Hamamatutyou",
         "Shinbasi","Yurakutyou" 
     };
 
     cout << "1970\n";
-    for (auto itr = yamanote.begin(); itr != yamanote.end(); ++itr) {
+    for (auto itr = yamanotesen.begin(); itr != yamanotesen.end(); ++itr) {
         cout << *itr << "\n";
     }
 
     cout << "\n";
 
     cout << "2019\n";
-    for (auto itr = yamanote.begin(); itr != yamanote.end(); ++itr) {
+    for (auto itr = yamanotesen.begin(); itr != yamanotesen.end(); ++itr) {
         if (*itr == "Tabata") {
-            itr = yamanote.insert(itr, "Nisinippori");
+            itr = yamanotesen.insert(itr, "Nisinippori");
             ++itr;
         }
     }
-    for (auto itr = yamanote.begin(); itr != yamanote.end(); ++itr) {
+    for (auto itr = yamanotesen.begin(); itr != yamanotesen.end(); ++itr) {
         cout << *itr << "\n";
     }
 
     cout << "\n";
 
     cout << "2022\n";
-    for (auto itr = yamanote.begin(); itr != yamanote.end(); ++itr) {
+    for (auto itr = yamanotesen.begin(); itr != yamanotesen.end(); ++itr) {
         if (*itr == "Tamati") {
-            itr = yamanote.insert(itr, "Takanawagettoway");
+            itr = yamanotesen.insert(itr, "Takanawagettoway");
             ++itr;
         }
     }
-    for (auto itr = yamanote.begin(); itr != yamanote.end(); ++itr) {
+    for (auto itr = yamanotesen.begin(); itr != yamanotesen.end(); ++itr) {
         cout << *itr << "\n";
     }
 
