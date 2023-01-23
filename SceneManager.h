0c56e8ae1696aae5b7f46993415cf931b01ae7d5
nagahama_kouki <k021g1319@g.neec.ac.jp>
2023-01-23 09:25:31 +0900

    add

---
+++ SceneManager.h
@@ -0,0 +1,13 @@
+#pragma once
+class SceneManager final
+{
+private:
+	SceneManager();
+	~SceneManager();
+
+public:
+	SceneManager(const SceneManager& obj) = delete;
+
+
+};
+
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<windows.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main() {
+
+
+
+
+    system("pause");
+    return 0;
+}
